de2917f9d1d38306c9847979b91394e93025b324
jih3954 <jih3954@gmail.com>
2017-09-29 14:01:48 +0900

    test에 출력 코드 추가

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Time is 12:44 PM");
+		System.out.println("I'm soooo hot");
 	}
 }
