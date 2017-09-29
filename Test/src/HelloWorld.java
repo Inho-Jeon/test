e752310ea196860224f97d5c594db507c0c2cfcc
jih3954 <jih3954@gmail.com>
2017-09-29 14:00:06 +0900

    출력문구추가

--- Test/src/HelloWorld.java
+++ Test/src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Time is 12:44 PM");
+		System.out.println("별이 빛나는 아름다운 밤이야이야");
 	}
 }
